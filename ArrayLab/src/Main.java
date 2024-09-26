9e2bf25a34303e8ccecbcab4dbd6de01c3fcfe4a
Justaz17 <159663429+Justaz17@users.noreply.github.com>
2024-09-26 14:18:47 +0100

    Added files

---
+++ ArrayLab/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) {
+        int[][] twoDementionalArray = new int[20][5];
+        int[] evenNumbers = {0,2,4,6,8};
+        int [] oddNumbers = {1,3,5,7,9};
+
+        for(int i =0; i < twoDementionalArray.length; i++)
+            if(i % 2 ==0)
+            {
+            }
+
+    }
+}
